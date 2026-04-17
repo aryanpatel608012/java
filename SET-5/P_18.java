f4f3035134bc5f27cef6159637bc9727f0c1e831
aryanpatel608012 <aryanpatel608012@gmail.com>
2026-04-17 09:55:10 +0530

    Add Vehicle hierarchy with Car and Bike classes

---
+++ SET-5/P_18.java
@@ -0,0 +1,34 @@
+abstract class Vehicle {
+    abstract String fuelType();
+    abstract int noOfWheels();
+}
+
+class Car extends Vehicle {
+    String fuelType() {
+        return "Petrol/Diesel";
+    }
+
+    int noOfWheels() {
+        return 4;
+    }
+}
+
+class Bike extends Vehicle {
+    String fuelType() {
+        return "Petrol";
+    }
+
+    int noOfWheels() {
+        return 2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car c = new Car();
+        Bike b = new Bike();
+
+        System.out.println(c.fuelType() + " " + c.noOfWheels());
+        System.out.println(b.fuelType() + " " + b.noOfWheels());
+    }
+}

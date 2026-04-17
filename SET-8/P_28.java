a36e11143ac16b5a39dac2c522cf69a8f71768d9
aryanpatel608012 <aryanpatel608012@gmail.com>
2026-04-17 10:02:07 +0530

    Add line, word, and character counter program
    This program reads a file named 'data.txt' and counts the number of lines, words, and characters, then prints the results.

---
+++ SET-8/P_28.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int lines = 0, words = 0, chars = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                lines++;
+                words += line.split("\\s+").length;
+                chars += line.replaceAll("\\s", "").length();
+            }
+
+            System.out.println("Lines: " + lines);
+            System.out.println("Words: " + words);
+            System.out.println("Characters: " + chars);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
